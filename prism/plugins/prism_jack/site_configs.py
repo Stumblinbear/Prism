1e8a4c3729fe8fba96d5e030f158c104e9bbbd59
Trevin Miller <stumblinbear@gmail.com>
2016-10-23 20:01:25 -0500

    Reworked Jack

--- prism/plugins/prism_jack/site_configs.py
+++ prism/plugins/prism_jack/site_configs.py
@@ -4,96 +4,97 @@ import prism
 
 from . import JackPlugin
 
-class ConfigWrapper:
-    def __init__(self, site_id, nginx_config):
-        self.site_id = site_id
-        self.nginx = nginx_config
-
-        if isinstance(nginx_config.children[0], nginx.Comment):
-            cmnt = nginx_config.children[0].comment
-            if cmnt[:7] == 'config=':
-                config = JackPlugin.get().get_default_config(cmnt[7:])
-                if config is not None:
-                    self.config = config
-        if self.config is None:
-            self.config = JackPlugin.get().get_default_config('nginx')
-
 class DefaultConfig:
     def __init__(self, type_id, description, options=[]):
         self.disabled = False
         self.type_id = type_id
         self.description = description
 
-        self.options = [('site_id', 'Site ID'), ('url_endpoint', 'URL Endpoint', 'example.com')] + options
+        self.options = [('site_id', 'Site ID')] + options
+
+    def generate(self, site_config, site_id):
+        pass
+
+    def delete(self, site_config):
+        pass
 
 class PHPConfig(DefaultConfig):
     def __init__(self):
-        DefaultConfig.__init__(self, 'php', 'Use this option if you wish to set up a website created using PHP.')
+        DefaultConfig.__init__(self, 'php', 'Use this option if you wish to set up a website created using PHP.', [('url_endpoint', 'URL Endpoint', 'example.com')])
         self.disabled = True
 
-    def generate_config(self, nginx_config, site_id, url_endpoint):
+    def generate(self, site_config, site_id, url_endpoint):
+        if not request.form['url_endpoint']:
+            return 'Must specify a URL endpoint.'
+        site_config['url_endpoint'] = request.form['url_endpoint']
+
+    def delete(self, site_config):
         pass
 
-    def post(self, request, nginx_config):
+    def post(self, request, site_config):
         pass
 
 class GUnicornConfig(DefaultConfig):
     def __init__(self):
-        DefaultConfig.__init__(self, 'gunicorn', 'Use this option if you wish to set up a website created using Python scripts. ')
+        DefaultConfig.__init__(self, 'gunicorn', 'Use this option if you wish to set up a website created using Python scripts.', [('url_endpoint', 'URL Endpoint', 'example.com')])
         self.disabled = True
 
-    def generate_config(self, nginx_config, site_id, url_endpoint):
+    def generate(self, site_config, site_id, url_endpoint):
+        if not request.form['url_endpoint']:
+            return 'Must specify a URL endpoint.'
+        site_config['url_endpoint'] = request.form['url_endpoint']
+
+    def delete(self, site_config):
         pass
 
-    def post(self, request, nginx_config):
+    def post(self, request, site_config):
         pass
 
 class ReverseProxyConfig(DefaultConfig):
     def __init__(self):
-        DefaultConfig.__init__(self, 'reverseproxy', 'Set up a website as a reverse proxy.', [('proxy_to', 'Proxy To', 'http://example.com/')])
+        DefaultConfig.__init__(self, 'reverseproxy', 'Set up a website as a reverse proxy.', [('url_endpoint', 'URL Endpoint', 'example.com'), ('proxy_to', 'Proxy To', 'http://example.com/')])
 
-    def generate_config(self, nginx_config, site_id, url_endpoint, proxy_to):
-        server_block = nginx.Server()
-        server_block.add(
-                        nginx.Key('listen', '80'),
-                        nginx.Key('listen', '[::]:80'),
-                        nginx.Key('server_name', url_endpoint),
-                        nginx.Location('/',
-                                nginx.Key('proxy_pass', proxy_to),
-                                nginx.Key('proxy_redirect', 'off'),
+    def generate(self, site_config, site_id, url_endpoint, proxy_to):
+        site_config['url_endpoint'] = url_endpoint
+        site_config['locations']['/'] = {
+                            'proxy_pass': proxy_to,
+                            'proxy_redirect': 'off',
 
-                                nginx.Key('proxy_set_header', 'Host $host'),
-                                nginx.Key('proxy_set_header', 'X-Real-IP $remote_addr'),
-                                nginx.Key('proxy_set_header', 'X-Forwarded-For $proxy_add_x_forwarded_for'),
+                            'proxy_set_header': (
+                                            'Host $host',
+                                            'X-Real-IP $remote_addr',
+                                            'X-Forwarded-For $proxy_add_x_forwarded_for'),
 
-                                nginx.Key('client_max_body_size', '10m'),
-                                nginx.Key('client_body_buffer_size', '128k'),
+                            'client_max_body_size': '10m',
+                            'client_body_buffer_size': '128k',
 
-                                nginx.Key('proxy_connect_timeout', '90'),
-                                nginx.Key('proxy_send_timeout', '90'),
-                                nginx.Key('proxy_read_timeout', '90'),
+                            'proxy_connect_timeout': '90',
+                            'proxy_send_timeout': '90',
+                            'proxy_read_timeout': '90',
 
-                                nginx.Key('proxy_buffer_size', '4k'),
-                                nginx.Key('proxy_buffers', '4 32k'),
-                                nginx.Key('proxy_busy_buffers_size', '64k'),
-                                nginx.Key('proxy_temp_file_write_size', '64k')
-                            )
-                    )
-        nginx_config.add(server_block)
-        return True
+                            'proxy_buffer_size': '4k',
+                            'proxy_buffers': '4 32k',
+                            'proxy_busy_buffers_size': '64k',
+                            'proxy_temp_file_write_size': '64k'
+                        }
 
-    def post(self, request, nginx_config):
+    def delete(self, site_config):
+        pass
+
+    def post(self, request, site_config):
+        if not request.form['url_endpoint']:
+            return 'Must specify a URL endpoint.'
         if not request.form['proxy_to']:
             return 'Must specify a site or IP to Proxy To.'
-        nginx_config.server.locations[0].keys[0].value = request.form['proxy_to']
+        site_config['url_endpoint'] = request.form['url_endpoint']
+        site_config['locations']['/']['proxy_pass'] = request.form['proxy_to']
 
 class AdvancedConfig(DefaultConfig):
     def __init__(self):
         DefaultConfig.__init__(self, 'nginx', 'Gives complete access to all configuration items.')
-        self.disabled = True
 
-    def generate_config(self, nginx_config, site_id, url_endpoint):
+    def delete(self, site_config):
         pass
 
-    def post(self, request, nginx_config):
+    def post(self, request, site_config):
         pass
--- prism/plugins/prism_jack/views.py
+++ prism/plugins/prism_jack/views.py
@@ -6,7 +6,6 @@ import prism
 from prism.api.view import BaseView, subroute
 
 from . import JackPlugin
-from .site_configs import ConfigWrapper
 
 
 class JackCreateSiteView(BaseView):
@@ -23,66 +22,50 @@ class JackCreateSiteView(BaseView):
             flask.flash('No site ID specified.', 'error')
             return ('jack.JackCreateSiteView')
 
-        if JackPlugin.get().get_config(request.form['site_id']):
-            flask.flash('A site with that ID already exists!', 'error')
-            return ('jack.JackCreateSiteView')
-
-        site_type = request.form['site_type']
-
-        site_config = JackPlugin.get().get_default_config(site_type)
-        if site_config == None:
+        default_config = JackPlugin.get().get_default_config(request.form['site_type'])
+        if default_config is None:
             flask.flash('Unknown config type!', 'error')
             return ('jack.JackCreateSiteView')
 
         options = []
-        for option in site_config.options:
+        for option in default_config.options:
             if not option[0] in request.form or not request.form[option[0]]:
                 flask.flash('Did not fill in all required values.', 'error')
                 return ('jack.JackCreateSiteView')
             options.append(request.form[option[0]])
 
-        nginx_config = nginx.Conf()
-        nginx_config.add(nginx.Comment('config=%s' % site_config.type_id))
-        if not site_config.generate_config(nginx_config, *options):
-            return ('jack.JackCreateSiteView')
-        nginx.dumpf(nginx_config, JackPlugin.get().get_config_loc(request.form['site_id']))
-
-        prism.os_command('systemctl restart nginx.service')
-
-        return ('core.RestartView')
+        site_config = JackPlugin.get().nginx.create_site(default_config, request.form['site_id'], options)
+        return ('jack.JackSiteOverviewView', {'site_uuid': site_config['uuid']})
 
 class JackSiteOverviewView(BaseView):
     def __init__(self):
         BaseView.__init__(self, endpoint='/view', title='View Site')
 
-    @subroute('/<site_id>')
-    def get(self, request, site_id):
-        if not JackPlugin.get().get_config(site_id):
+    @subroute('/<site_uuid>')
+    def get(self, request, site_uuid):
+        if site_uuid not in JackPlugin.get().nginx.configs:
             flask.flash('A site with that ID doesn\'t exist!', 'error')
             return ('dashboard.DashboardView')
-        wrapper = ConfigWrapper(site_id, JackPlugin.get().get_config(site_id))
-        return ('view/%s.html' % wrapper.config.type_id, {'config': wrapper})
+        config = JackPlugin.get().nginx.configs[site_uuid]
+        return ('view/%s.html' % config['type'], {'config': config, 'tabs': JackPlugin.get()._site_tabs})
 
-    @subroute('/<site_id>')
-    def post(self, request, site_id):
-        if not JackPlugin.get().get_config(site_id):
+    @subroute('/<site_uuid>')
+    def post(self, request, site_uuid):
+        if not JackPlugin.get().nginx.configs[site_uuid]:
             flask.flash('A site with that ID doesn\'t exist!', 'error')
             return ('dashboard.DashboardView')
 
-        if 'delete' in request.form:
-            os.remove(os.path.join(JackPlugin.get().config('sites-loc', '/etc/nginx/conf.d/'), site_id + '.conf'))
-        else:
+        if 'tab' in request.form:
+            pass
+        elif 'delete' in request.form:
+            JackPlugin.get().nginx.delete_site(site_uuid)
+            return ('dashboard.DashboardView')
+        elif 'update' in request.form:
             if not request.form['url_endpoint']:
                 error = 'Must specify a URL endpoint.'
             else:
-                wrapper = ConfigWrapper(site_id, JackPlugin.get().get_config(site_id))
-                wrapper.nginx.server.keys[2].value = request.form['url_endpoint']
-                error = wrapper.config.post(request, wrapper.nginx)
+                site_config = JackPlugin.get().nginx.configs[site_uuid]
+                error = JackPlugin.get().get_default_config(site_config['type']).post(request, site_config)
             if error:
                 flask.flash(error, 'error')
-                return ('jack.JackSiteOverviewView', {'site_id': site_id})
-            else:
-                nginx.dumpf(wrapper.nginx, JackPlugin.get().get_config_loc(site_id))
-
-        prism.os_command('systemctl restart nginx.service')
-        return ('core.RestartView')
+        return ('jack.JackSiteOverviewView', {'site_uuid': site_uuid})
