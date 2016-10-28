1c1ea60323664ec7d50a370309a90218ebf32ef7
root <stumblinbear@gmail.com>
2016-10-28 18:22:25 -0400

    Logging to file

--- prism/logging.py
+++ prism/logging.py
@@ -1,3 +1,7 @@
+import re
+
+open('/var/log/prism.log', 'w').close()
+
 levels = 0
 def up(string=None):
 	if string is not None:
@@ -13,6 +17,8 @@ def output(string='', prefix_color='\033[90m', prefix_char=':', extend_char='|',
 	for i in range(0, levels):
 		prefix = prefix + extend_char
 	print('\033[1m' + prefix_color + prefix + '\033[0m' + string_color + str(string) + '\033[0m')
+	with open('/var/log/prism.log', 'a') as f:
+		f.write(re.sub('\x1b[^m]*m', '', prefix + str(string)) + '\n')
 
 def info(string):
 	output(string, '\033[34m', 'i', '|', '\033[94m')
